c5c7c7170f12ef14a09ba24f9a8611facd4ec46e
chokoladis <igor2011__igor@mail.ru>
2026-02-18 23:04:30 +0400

    prepare skeleton

---
+++ basic/views/site/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<form action="">
+    <input type="text" class="form-control" id="url">
+    <button type="button" class="btn btn-primary">Отправить</button>
+</form>
